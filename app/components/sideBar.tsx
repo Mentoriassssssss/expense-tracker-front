e42c7f3f7b6aef9cf31c87be1eb69a87e799161f
Mentoriassssssss <pghuy18@gmail.com>
2024-09-06 14:25:08 +0700

    reinitialized front end

---
+++ app/components/sideBar.tsx
@@ -0,0 +1,17 @@
+export default function SideBar() {
+    return (
+        <div className="
+        h-full
+        w-1/6
+        bg-[var(--background)]
+        border-2
+        border-[var(--border)]
+        rounded-3xl
+        p-4
+        text-[var(--primary-color)]
+        font-bold
+        ">
+            NavBar
+        </div>
+    )
+}
